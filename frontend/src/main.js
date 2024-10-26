46eda167abc857db2051abc2d522f86b1db3a20d
mmmurka <murenkodan2005@gmail.com>
2024-10-26 11:46:20 +0300

    updated support android.5(not stable)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';  // Импортируем маршрутизатор
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 createApp(App)
   .use(router)  // Используем маршрутизатор
